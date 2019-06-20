aecc98de07e9d4673e246356369056d2ad198cab
arisamuto <cookiemonster.ca17@gmail.com>
2019-06-21 05:28:27 +0900

    working

---
+++ thisisfinal/MemoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MemoViewController.swift
+//  thisisfinal
+//
+//  Created by Arisa Muto on 2019/06/21.
+//  Copyright © 2019 Arisa Muto. All rights reserved.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+    @IBOutlet var text: UITextField!
+    let saveData = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
