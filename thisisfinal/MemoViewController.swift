993ae600aaaa029a47d3b8b0c9ed2db76e3397b7
arisamuto <cookiemonster.ca17@gmail.com>
2019-06-23 00:08:42 +0900

    もう少し！

--- thisisfinal/MemoViewController.swift
+++ thisisfinal/MemoViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class MemoViewController: UIViewController, UITableViewDataSource,UITableViewDelegate  {
     
     @IBOutlet weak var memoTableView: UITableView!
+    
     var memoArray = [String]()
     let ud = UserDefaults.standard
     
