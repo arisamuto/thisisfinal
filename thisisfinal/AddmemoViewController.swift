1a014d140b22cb5292efe01b07338cdb505d6efe
arisamuto <cookiemonster.ca17@gmail.com>
2019-06-22 19:34:57 +0900

    working

---
+++ thisisfinal/AddmemoViewController.swift
@@ -0,0 +1,51 @@
+//
+//  AddmemoViewController.swift
+//  thisisfinal
+//
+//  Created by Arisa Muto on 2019/06/21.
+//  Copyright © 2019 Arisa Muto. All rights reserved.
+//
+
+import UIKit
+
+class AddmemoViewController: UIViewController {
+
+    @IBOutlet var memoTextview: UITextView!
+    var text1: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func save(_sender: Any) {
+        
+        let inputText = memoTextview.text
+        let ud = UserDefaults.standard
+        if ud.array(forKey: "memoArray") != nil {
+            //saveMemoArrayに取得
+            var saveMemoArray = ud.array(forKey: "memoArrat") as![String]
+            if inputText != ""{
+                saveMemoArray.append(inputText!)
+                ud.set(saveMemoArray, forKey: "memoArray")
+            }else{
+               let alert = UIAlertController(
+                title: "TextBOX", message: "何も入力されていません", preferredStyle: .alert)
+        }
+        
+        }else{
+            //最初何も書かれていない場合
+            var newMemoArray = [String]()
+            //nilを強制アンラップはエラーが出るから
+            if inputText != ""{
+                //inputtextはoptional型だから強制アンラップ
+                newMemoArray.append(inputText!)
+                ud.set(newMemoArray, forKey: "memoArray")
+            }else{
+                let alert = UIAlertController(
+                    title: "保存完了", message: "Good Joob!!", preferredStyle: .alert)
+                ud.synchronize()
+        }
+
+}
+}
+}
---
+++ thisisfinal/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  thisisfinal
+//
+//  Created by Arisa Muto on 2019/06/21.
+//  Copyright © 2019 Arisa Muto. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
