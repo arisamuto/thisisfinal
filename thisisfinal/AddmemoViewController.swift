481b98337baae758f814db640370d6a347250b6f
arisamuto <cookiemonster.ca17@gmail.com>
2019-06-26 00:04:46 +0900

    保存の表示

--- thisisfinal/AddmemoViewController.swift
+++ thisisfinal/AddmemoViewController.swift
@@ -10,25 +10,29 @@ import UIKit
 
 class AddmemoViewController: UIViewController {
     
-    @IBOutlet var memoTextview: UITextView!
     @IBOutlet var back2: UIButton!
+    @IBOutlet var memoTextview: UITextView!
     var text1: String!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //ステイタスバーを非表示
         self.setNeedsStatusBarAppearanceUpdate()
+        //ボタン
         back2.layer.borderColor = UIColor.blue.cgColor
         back2.layer.borderWidth = 1.0
         back2.layer.cornerRadius = 10
+        
     }
     
     @IBAction func save(_sender: Any) {
         
         let inputText = memoTextview.text
         let ud = UserDefaults.standard
-        
+        let alert = UIAlertController(
+            title: "保存完了", message: "メモ追加しました", preferredStyle: .alert
+        )
+   
         
         if ud.array(forKey: "memoArray") != nil{
             var saveMemoArray = ud.array(forKey: "memoArray") as! [String]
@@ -50,14 +54,10 @@ class AddmemoViewController: UIViewController {
             ud.set(newMemoArray, forKey: "memoArray")
         }
         ud.synchronize()
-        let alert = UIAlertController(
-            title: "保存完了", message: "メモ追加しました", preferredStyle: .alert
-        )
         present(alert,animated: true, completion: nil)
-        
+        self.dismiss(animated: true, completion: nil)
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
- 
 }
